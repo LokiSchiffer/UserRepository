00f0de07626f40d2a1a10247ea7d88f6156a14c6
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-04-03 16:57:56 -0500

    Project initialization

---
+++ src/main/java/com/example/UserRepository/UserRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRepositoryApplication.class, args);
+	}
+
+}
