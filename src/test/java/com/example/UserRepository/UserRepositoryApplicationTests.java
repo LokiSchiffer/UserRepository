72ed9e2a729c4cfeb6632e327c9d32b60a2439c4
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-04-06 14:23:12 -0500

    Prepare Test

--- src/test/java/com/example/UserRepository/UserRepositoryApplicationTests.java
+++ src/test/java/com/example/UserRepository/UserRepositoryApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.UserRepository;
 
+import com.example.UserRepository.Model.User;
+import com.example.UserRepository.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
-class UserRepositoryApplicationTests {
+class  UserRepositoryApplicationTests {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
-	void contextLoads() {
+	void WhenSavingNewProject_thenSuccess() {
+		User newUser = new User(randomAlphabetic(16), randomAlphabetic(6),
+				randomAlphabetic(16), 123456789);
+		assertNotNull(userRepository.save(newUser));
 	}
 
 }
