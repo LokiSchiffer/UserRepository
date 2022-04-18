b71e7b991eafcf17093757c5e3c1a3d2e1ee963e
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-04-18 09:25:36 -0500

    Implement annotations

--- src/main/java/com/example/UserRepository/Model/User.java
+++ src/main/java/com/example/UserRepository/Model/User.java
@@ -1,16 +1,30 @@
 package com.example.UserRepository.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User {
 
     @Id
+    @Email
     private String email;
 
+    @Column(nullable = false)
+    @Size(max = 50, message = "Firstname out of size")
+    @NotNull
     private String firstName;
+
+    @Column(nullable = false)
+    @Size(max = 50)
+    @NotNull
     private String lastName;
+
+    @Column(nullable = false)
     private long phoneNumber;
 
     protected User() {
--- src/main/java/com/example/UserRepository/Repository/UserRepository.java
+++ src/main/java/com/example/UserRepository/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.UserRepository.Repository;
 
 import com.example.UserRepository.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
