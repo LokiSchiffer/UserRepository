7a921f8cc037e6ad2e33f40f3ccef31f9fef81f3
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-04-19 08:57:50 -0500

    Update package line in webconfig class

--- src/main/java/com/example/UserRepository/web/config/WebConfig.java
+++ src/main/java/com/example/UserRepository/web/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.UserRepository.web.controller.config;
+package com.example.UserRepository.web.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
