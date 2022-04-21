e1e104790a7f752b6a25ba03c6f2463b44ca79b0
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-04-21 10:23:47 -0500

    Add exceptions tests in userServiceImplTest

--- src/test/java/com/example/UserRepository/UserServiceImplTests.java
+++ src/test/java/com/example/UserRepository/UserServiceImplTests.java
@@ -1,22 +1,21 @@
 package com.example.UserRepository;
 
 import com.example.UserRepository.logic.dto.UserDto;
+import com.example.UserRepository.logic.exceptions.MyUserException;
 import com.example.UserRepository.logic.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@Transactional
-@Rollback
 public class UserServiceImplTests {
 
 	@Autowired
@@ -32,7 +31,7 @@ public class UserServiceImplTests {
 
 	@Test
 	void updateUserTest() {
-		UserDto newUser = new UserDto("prueba1@mail.com", randomAlphabetic(6),
+		UserDto newUser = new UserDto("prueba2@mail.com", randomAlphabetic(6),
 				randomAlphabetic(16), "+50312345678");
 		userService.createUser(newUser);
 		UserDto secondUser = new UserDto();
@@ -46,10 +45,74 @@ public class UserServiceImplTests {
 
 	@Test
 	void findUserTest(){
-		UserDto newUser = new UserDto("prueba1@mail.com", randomAlphabetic(6),
+		UserDto newUser = new UserDto("prueba3@mail.com", randomAlphabetic(6),
 				randomAlphabetic(16), "+50312345678");
 		userService.createUser(newUser);
 		UserDto userCreated = userService.findUser(newUser.getEmail());
 		assertThat(userCreated, is(equalTo(newUser)));
+	}
+
+	@Test
+	void duplicateUserCreationExceptionTest(){
+		UserDto newUser = new UserDto("prueba4@mail.com", randomAlphabetic(6),
+				randomAlphabetic(16), "+50312345678");
+		userService.createUser(newUser);
+		MyUserException exception = assertThrows(MyUserException.class, () -> {
+			userService.createUser(newUser);
+		});
+		assertThat(exception.getClass(), is(equalTo(MyUserException.class)));
+	}
+
+	@Test
+	void updateUserWithWrongMailExceptionTest() {
+		UserDto newUser = new UserDto("prueba5@mail.com", randomAlphabetic(6),
+				randomAlphabetic(16), "+50312345678");
+		userService.createUser(newUser);
+		UserDto secondUser = new UserDto();
+		secondUser.setEmail(newUser.getEmail());
+		secondUser.setFirstName("Loki");
+		secondUser.setLastName("Schiffer");
+		secondUser.setPhoneNumber(newUser.getPhoneNumber());
+		MyUserException exception = assertThrows(MyUserException.class, () -> {
+			userService.updateUser("prueba@mail.com", secondUser);
+		});
+		assertThat(exception.getClass(), is(equalTo(MyUserException.class)));
+	}
+
+	@Test
+	void updateUserWithDifferentMailsExceptionTest() {
+		UserDto newUser = new UserDto("prueba6@mail.com", randomAlphabetic(6),
+				randomAlphabetic(16), "+50312345678");
+		userService.createUser(newUser);
+		newUser = new UserDto("prueba7@mail.com", randomAlphabetic(6),
+				randomAlphabetic(16), "+50312345678");
+		userService.createUser(newUser);
+		UserDto secondUser = new UserDto();
+		secondUser.setEmail(newUser.getEmail());
+		secondUser.setFirstName("Loki");
+		secondUser.setLastName("Schiffer");
+		secondUser.setPhoneNumber(newUser.getPhoneNumber());
+		MyUserException exception = assertThrows(MyUserException.class, () -> {
+			userService.updateUser("prueba6@mail.com", secondUser);
+		});
+		assertThat(exception.getClass(), is(equalTo(MyUserException.class)));
+	}
+
+	@Test
+	void findUserExceptionTest() {
+		MyUserException exception = assertThrows(MyUserException.class, () -> {
+			assertNotNull(userService.findUser("prueba@mail.com"));
+		});
+		assertThat(exception.getClass(), is(equalTo(MyUserException.class)));
+	}
+
+	@Test
+	void dataIntegrityViolationExceptionTest(){
+		UserDto newUser = new UserDto("prueba8@mail.com", null,
+				randomAlphabetic(16), "+50312345678");
+		DataIntegrityViolationException exception = assertThrows(DataIntegrityViolationException.class, () -> {
+			userService.createUser(newUser);
+		});
+		assertThat(exception.getClass(), is(equalTo(DataIntegrityViolationException.class)));
 	}
 }
